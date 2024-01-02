c58814c3d691fa055f8dc04c983ac7643c6d3b42
maria5237 <152502129+maria5237@users.noreply.github.com>
2024-01-02 23:59:29 +0700

    Update main9.py

--- main9.py
+++ main9.py
@@ -1,31 +1,55 @@
 import pygame
 
 
-class Fighter():
-    def __init__(self, x, y):
+class Fighter:
+    def __init__(self, x, y, data, sprite_sheet, animation_steps):
+        self.size = data[0]
+        self.hight = data[1]
+        self.image_scale = data[2]
+        self.offset = data[3]
+        self.flip = False
+        self.animation_list = self.load_images(sprite_sheet, animation_steps)
+        self.action = 0 #0:на месте #1:прыжок #2:бег #3:смерть #4:атака_1 #5:удар
+        self.frame_index = 0
+        self.image = self.animation_list[self.action][self.frame_index]
+        self.update_time = pygame.time.get_ticks()
         self.rect = pygame.Rect((x, y, 80, 180))
         self.vel_y = 0
+        self.running = False
         self.jump = False
         self.attacking = False
         self.attack_type = 0
         self.health = 100
+
+    def load_images(self, sprite, animation_steps):
+        #извлечение изображения из таблицы спрайтов
+        animation_list = []
+        for y, j in enumerate(animation_steps):
+            temp_img_list = []
+            for i in range(j):
+                temp_img = sprite.subsurface(i * self.size, y * self.hight, self.size, self.hight)
+                temp_img_list.append(pygame.transform.scale(temp_img, (self.size * self.image_scale, self.hight * self.image_scale)))
+            animation_list.append(temp_img_list)
+        return animation_list
 
     def move(self, sc_w, sc_h, surface, target):
         speed = 10
         gravity = 2
         dx = 0
         dy = 0
+        self.running = False
 
         #получать нажатия клавиш
         key = pygame.key.get_pressed()
-
 
         #может выполнять другие действия только в том случае, если в данный момент не атакует
         if self.attacking == False:
             if key[pygame.K_a]:
                 dx = -speed
+                self.running = True
             if key[pygame.K_d]:
                 dx = speed
+                self.running = True
             # прыгать
             if key[pygame.K_w] and self.jump == False:
                 self.vel_y = -30
@@ -55,18 +79,46 @@ class Fighter():
             self.jump = False
             dy = sc_h - 70 - self.rect.bottom
 
+        #убедиться, что игроки смотрят друг на друга
+        if target.rect.centerx > self.rect.centerx:
+            self.flip = False
+        else:
+            self.flip = True
 
         #обновления позиции игрока
         self.rect.x += dx
         self.rect.y += dy
 
+    #обрабатывать обновления анимации
+    def update(self):
+        #какое действие выполняет игрок
+        if self.running == True:
+            self.update_action(2)
+        else:
+            self.update_action(0)
+        time = 100
+        self.image = self.animation_list[self.action][self.frame_index]
+        if pygame.time.get_ticks() - self.update_time > time:
+            self.frame_index += 1
+            self.update_time = pygame.time.get_ticks()
+        if self.frame_index >= len(self.animation_list[self.action]):
+            self.frame_index = 0
+
     def attack(self, surface, target):
         self.attacking = True
-        attacking_rect = pygame.Rect(self.rect.centerx, self.rect.y, 2 * self.rect.width, self.rect.height)
+        attacking_rect = pygame.Rect(self.rect.centerx - (2 * self.rect.width * self.flip), self.rect.y, 2 * self.rect.width, self.rect.height)
         if attacking_rect.colliderect(target.rect):
             target.health -= 10
 
         pygame.draw.rect(surface, (0, 255, 0), attacking_rect)
 
+    def update_action(self, new_action):
+        if new_action != self.action:
+            self.action = new_action
+            self.frame_index = 0
+            self.update_time = pygame.time.git_ticks()
+
     def draw(self, surface):
+        img = pygame.transform.flip(self.image, self.flip, False)
         pygame.draw.rect(surface, (255, 0, 0), self.rect)
+        surface.blit(img, (self.rect.x - (self.offset[0] * self.image_scale), self.rect.y - (self.offset[0] * self.image_scale)))
