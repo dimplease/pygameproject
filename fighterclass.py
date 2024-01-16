dad97479721b316286e808f3a596875c6d90dca8
eva <eva580109@yandex.ru>
2024-01-16 23:01:23 +0700

    update

---
+++ fighterclass.py
@@ -0,0 +1,145 @@
+import pygame
+
+
+class Fighter():
+    def __init__(self, player, x, y, flip, data, sprite, steps):
+        self.player = player
+        self.size = data[0]
+        self.imagescale = data[1]
+        self.offset = data[2]
+        self.animationlist = self.images(sprite, steps)
+        self.action = 0 #0:на месте #1:бег #2:прыжок #3:атака #4:смерть
+        self.frame = 0
+        self.update = pygame.time.get_ticks()
+        self.image = self.animationlist[self.action][self.frame]
+        self.flip = flip
+        self.rect = pygame.Rect((x, y, 80, 180))
+        self.yspeed = 0
+        self.attackingcooldown = 0
+        self.jumppossibility = False
+        self.attackingtype = 0
+        self.attacking = False
+        self.xp = 100
+        self.running = False
+        self.alive = True
+
+    def images(self, sprite, steps):
+        #извлечение изображения из таблицы спрайтов
+        animationlist = []
+        for y, animation in enumerate(steps):
+            temp_img = []
+            for i in range(animation):
+                temp = sprite.subsurface(i * self.size, y * self.size, self.size, self.size)
+                temp_img.append(pygame.transform.scale(temp, (self.size * self.imagescale, self.size * self.imagescale)))
+            animationlist.append(temp_img)
+        return animationlist
+
+    def draw(self, surface):
+        img = pygame.transform.flip(self.image, self.flip, False)
+        surface.blit(img, (self.rect.x - (self.offset[0] * self.imagescale), self.rect.y - (self.offset[1] * self.imagescale)))
+
+    def attack(self, target):
+        if self.attackingcooldown == 0:
+            self.attacking = True
+            attackingrect = pygame.Rect(self.rect.centerx - (2 * self.rect.width * self.flip), self.rect.y, 2 * self.rect.width, self.rect.height)
+            if attackingrect.colliderect(target.rect):
+                target.xp -= 10
+
+    #обрабатывать обновления анимации
+    def updating(self):
+        if self.xp <= 0:
+            self.xp = 0
+            self.alive = False
+            self.update_action(4)
+        elif self.attacking is True:
+            self.update_action(3)
+        elif self.jumppossibility == True:
+            self.update_action(2)
+        elif self.running == True:
+            self.update_action(1)
+        else:
+            self.update_action(0)
+        animationcooldown = 150
+        self.image = self.animationlist[self.action][self.frame]
+        if pygame.time.get_ticks() - self.update > animationcooldown:
+            self.frame += 1
+            self.update = pygame.time.get_ticks()
+        if self.frame >= len(self.animationlist[self.action]):
+            if self.alive == False:
+                self.frame = len(self.animationlist[self.action]) - 1
+            else:
+                self.frame = 0
+        if self.action == 3:
+            self.attacking = False
+            self.attackingcooldown = 20
+
+    def moving(self, surface, target, gameover):
+        speed = 8
+        xcoor = 0
+        ycoor = 0
+        grav = 2
+        self.running = False
+        self.attackingtype = 0
+        #получать нажатия клавиш
+        key = pygame.key.get_pressed()
+        #выполнение других действий 
+        if self.attacking is False and self.alive is True and gameover is False:
+            if self.player == 1:
+                if key[pygame.K_LEFT]:
+                    xcoor = -speed
+                    self.running = True
+                if key[pygame.K_RIGHT]:
+                    xcoor = speed
+                    self.running = True
+                if key[pygame.K_UP] and self.jumppossibility is False:
+                    self.yspeed = -30
+                    self.jumppossibility = True
+                if key[pygame.K_l]:
+                    self.attack(target)
+                    self.action = 3
+            if self.player == 2:
+                if key[pygame.K_a]:
+                    xcoor = -speed
+                    self.running = True
+                if key[pygame.K_d]:
+                    xcoor = speed
+                    self.running = True
+                if key[pygame.K_w] and self.jumppossibility is False:
+                    self.yspeed = -30
+                    self.jumppossibility = True
+                if key[pygame.K_r]:
+                    self.attack(target)
+                    self.action = 3
+                    
+        #применение гравитации
+        self.yspeed += grav
+        ycoor += self.yspeed
+        
+        #игрок остается на экране
+        if self.rect.left + xcoor < 0:
+            xcoor = -self.rect.left
+        if self.rect.right + xcoor > 1000:
+            xcoor = 1000 - self.rect.right
+        if self.rect.bottom + ycoor > 490:
+            self.yspeed = 0
+            self.jumppossibility = False
+            ycoor = 490 - self.rect.bottom
+            
+        #убедиться, что игроки смотрят друг на друга
+        if target.rect.centerx > self.rect.centerx:
+            self.flip = False
+        else:
+            self.flip = True
+
+        if self.attackingcooldown > 0:
+            self.attackingcooldown -= 1
+
+        #обновления позиции игрока
+        self.rect.x += xcoor
+        self.rect.y += ycoor
+
+    def update_action(self, newaction):
+        if newaction != self.action:
+            self.action = newaction
+            self.frame = 0
+            self.update = pygame.time.get_ticks()
--- main.py
+++ main.py
@@ -1,145 +1,116 @@
 import pygame
+from fighterclass import Fighter
+
+#создание окна игры
+pygame.init()
+screen_width = 1000
+screen_height = 600
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Fighting game')
+
+#установить частоту кадров
+clock = pygame.time.Clock()
+
+counter = 3
+lastcounter = pygame.time.get_ticks()
+score = [0, 0]
+gameover = False
+gameovercooldown = 2000
+
+#определите переменные истребителя
+codysize = 60
+codyscale = 5
+codyoffset = [35, 10]
+codydata = [codysize, codyscale, codyoffset]
+shadowsize = 70
+shadowscale = 3.5
+shadowoffset = [20, 5]
+shadowdata = [shadowsize, shadowscale, shadowoffset]
+
+#загрузка музыки
+pygame.mixer.music.load('data/music/sergey.mp3')
+pygame.mixer.music.set_volume(0.2)
+pygame.mixer.music.play(-1, 0.0, 5000)
+
+#загрузка фона игры
+background = pygame.image.load('data/pictures/background.jpg').convert_alpha()
+#загружать таблицы спрайтов
+cody_sheet = pygame.image.load('data/pictures/codymain2.png').convert_alpha()
+shadow_sheet = pygame.image.load('data/pictures/shadowmain2.png').convert_alpha()
+victorypicture = pygame.image.load('data/pictures/victory.png').convert_alpha()
+#определение количества шагов в анимации
+codysteps = [6, 4, 3, 6, 1]
+shadowsteps = [3, 3, 3, 5, 1]
 
 
-class Fighter():
-    def __init__(self, player, x, y, flip, data, sprite, steps):
-        self.player = player
-        self.size = data[0]
-        self.imagescale = data[1]
-        self.offset = data[2]
-        self.animationlist = self.images(sprite, steps)
-        self.action = 0 #0:на месте #1:бег #2:прыжок #3:атака #4:смерть
-        self.frame = 0
-        self.update = pygame.time.get_ticks()
-        self.image = self.animationlist[self.action][self.frame]
-        self.flip = flip
-        self.rect = pygame.Rect((x, y, 80, 180))
-        self.yspeed = 0
-        self.attackingcooldown = 0
-        self.jumppossibility = False
-        self.attackingtype = 0
-        self.attacking = False
-        self.xp = 100
-        self.running = False
-        self.alive = True
+#функция рисования фона
+def drawing_background():
+    scaled_background = pygame.transform.scale(background, (screen_width, screen_height))
+    screen.blit(scaled_background, (0, 0))
 
-    def images(self, sprite, steps):
-        #извлечение изображения из таблицы спрайтов
-        animationlist = []
-        for y, animation in enumerate(steps):
-            temp_img = []
-            for i in range(animation):
-                temp = sprite.subsurface(i * self.size, y * self.size, self.size, self.size)
-                temp_img.append(pygame.transform.scale(temp, (self.size * self.imagescale, self.size * self.imagescale)))
-            animationlist.append(temp_img)
-        return animationlist
 
-    def draw(self, surface):
-        img = pygame.transform.flip(self.image, self.flip, False)
-        surface.blit(img, (self.rect.x - (self.offset[0] * self.imagescale), self.rect.y - (self.offset[1] * self.imagescale)))
+countfont = pygame.font.Font('data/text font/turok.ttf', 80)
 
-    def attack(self, target):
-        if self.attackingcooldown == 0:
-            self.attacking = True
-            attackingrect = pygame.Rect(self.rect.centerx - (2 * self.rect.width * self.flip), self.rect.y, 2 * self.rect.width, self.rect.height)
-            if attackingrect.colliderect(target.rect):
-                target.xp -= 10
 
-    #обрабатывать обновления анимации
-    def updating(self):
-        if self.xp <= 0:
-            self.xp = 0
-            self.alive = False
-            self.update_action(4)
-        elif self.attacking is True:
-            self.update_action(3)
-        elif self.jumppossibility == True:
-            self.update_action(2)
-        elif self.running == True:
-            self.update_action(1)
-        else:
-            self.update_action(0)
-        animationcooldown = 150
-        self.image = self.animationlist[self.action][self.frame]
-        if pygame.time.get_ticks() - self.update > animationcooldown:
-            self.frame += 1
-            self.update = pygame.time.get_ticks()
-        if self.frame >= len(self.animationlist[self.action]):
-            if self.alive == False:
-                self.frame = len(self.animationlist[self.action]) - 1
-            else:
-                self.frame = 0
-        if self.action == 3:
-            self.attacking = False
-            self.attackingcooldown = 20
+def drawtext(text, font, text_col, x, y):
+    img = font.render(text, True, text_col)
+    screen.blit(img, (x, y))
 
-    def moving(self, surface, target, gameover):
-        speed = 8
-        xcoor = 0
-        ycoor = 0
-        grav = 2
-        self.running = False
-        self.attackingtype = 0
-        #получать нажатия клавиш
-        key = pygame.key.get_pressed()
-        #выполнение других действий 
-        if self.attacking is False and self.alive is True and gameover is False:
-            if self.player == 1:
-                if key[pygame.K_LEFT]:
-                    xcoor = -speed
-                    self.running = True
-                if key[pygame.K_RIGHT]:
-                    xcoor = speed
-                    self.running = True
-                if key[pygame.K_UP] and self.jumppossibility is False:
-                    self.yspeed = -30
-                    self.jumppossibility = True
-                if key[pygame.K_l]:
-                    self.attack(target)
-                    self.action = 3
-            if self.player == 2:
-                if key[pygame.K_a]:
-                    xcoor = -speed
-                    self.running = True
-                if key[pygame.K_d]:
-                    xcoor = speed
-                    self.running = True
-                if key[pygame.K_w] and self.jumppossibility is False:
-                    self.yspeed = -30
-                    self.jumppossibility = True
-                if key[pygame.K_r]:
-                    self.attack(target)
-                    self.action = 3
-                    
-        #применение гравитации
-        self.yspeed += grav
-        ycoor += self.yspeed
-        
-        #игрок остается на экране
-        if self.rect.left + xcoor < 0:
-            xcoor = -self.rect.left
-        if self.rect.right + xcoor > 1000:
-            xcoor = 1000 - self.rect.right
-        if self.rect.bottom + ycoor > 490:
-            self.yspeed = 0
-            self.jumppossibility = False
-            ycoor = 490 - self.rect.bottom
-            
-        #убедиться, что игроки смотрят друг на друга
-        if target.rect.centerx > self.rect.centerx:
-            self.flip = False
-        else:
-            self.flip = True
 
-        if self.attackingcooldown > 0:
-            self.attackingcooldown -= 1
+#функция рисования полосок здоровья бойца
+def xp_bar(xp, x, y):
+    ratio = xp / 100
+    pygame.draw.rect(screen, (255, 0, 0), (x, y, 400, 30))
+    pygame.draw.rect(screen, (255, 255, 255), (x - 2, y - 2, 404, 34))
+    pygame.draw.rect(screen, (255, 255, 0), (x, y, ratio * 400, 30))
 
-        #обновления позиции игрока
-        self.rect.x += xcoor
-        self.rect.y += ycoor
+#создание двух экземпляров бойцов
+firstfighter = Fighter(1, 200, 310, False, codydata, cody_sheet, codysteps)
+secondfighter = Fighter(2, 700, 310, True, shadowdata, shadow_sheet, shadowsteps)
+action = True
+while action:
+    clock.tick(60)
+    #рисунок фона
+    drawing_background()
+    #показатель здоровье игрока
+    xp_bar(firstfighter.xp, 20, 20)
+    xp_bar(secondfighter.xp, 580, 20)
+    #перемещение бойцов
+    if counter <= 0:
+        firstfighter.moving(screen, secondfighter, gameover)
+        secondfighter.moving(screen, firstfighter, gameover)
+    else:
+        drawtext(str(counter), countfont, (255, 0, 0), 500, 200)
+        if (pygame.time.get_ticks() - lastcounter) >= 1000:
+            counter -= 1
+            lastcounter = pygame.time.get_ticks()
+    #обновление бойцов
+    firstfighter.updating()
+    secondfighter.updating()
+    #рисунок бойцов
+    firstfighter.draw(screen)
+    secondfighter.draw(screen)
 
-    def update_action(self, newaction):
-        if newaction != self.action:
-            self.action = newaction
-            self.frame = 0
-            self.update = pygame.time.get_ticks()
+    if gameover is False:
+        if firstfighter.alive == False:
+            score[1] += 1
+            gameover = True
+            gameovertime = pygame.time.get_ticks()
+        elif secondfighter.alive == False:
+            score[0] += 1
+            gameover = True
+            gameovertime = pygame.time.get_ticks()
+    else:
+        screen.blit(victorypicture, (360, 150))
+        if pygame.time.get_ticks() - gameovertime > gameovercooldown:
+            gameover = False
+            counter = 3
+            firstfighter = Fighter(1, 200, 310, False, codydata, cody_sheet, codysteps)
+            secondfighter = Fighter(2, 700, 310, True, shadowdata, shadow_sheet, shadowsteps)
+    #обработчик события
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            action = False
+
+    pygame.display.update()
+pygame.quit()
